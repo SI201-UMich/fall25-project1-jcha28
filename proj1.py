decb35299f949ac4a34e7ea182decd8c728229a9
Jenny Cha <jennycha@Jennys-MacBook-Air-7.local>
2025-10-09 19:27:12 -0400

    def avg_bodymass 1

--- proj1.py
+++ proj1.py
@@ -6,8 +6,27 @@ email: jennycha@umich.edu
 
 '''
 
+import csv #using penguins dataset
 
-import unittest
 
+def read_penguin_data(filename):
+    data = []
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            data.apend(row)
+    return data
 
-def count_words(s):
+def avg_bodymass(data):
+    results = []
+    totals = {}
+    counts = {}
+
+    for i in range(1, data):
+        row = data[i]
+        species = row[0]
+        sex = row[i+1]
+        body_mass = row[i-1]
+
+        if body_mass == '' or sex == '':
+
