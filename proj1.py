0d47a83d3e8a3bc17f8b90073f2055e7f50e3438
Jenny Cha <jennycha@Jennys-MacBook-Air-7.local>
2025-10-14 00:45:17 -0400

    def test for calculation 2, general test case 1

--- proj1.py
+++ proj1.py
@@ -9,15 +9,15 @@ email: jennycha@umich.edu
 import csv #using penguins dataset
 
 
-def read_penguin_data(filename):
+def read_penguin_data(filename): #reads/analyzes penguin data from csv file
     data = []
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile)
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
         for row in reader:
-            data.apend(row)
+            data.append(row)
     return data
 
-def avg_bodymass(data):
+def avg_bodymass(data): #calculation 1: what's the average body mass by species and sex
     results = []
     totals = {}
     counts = {}
@@ -28,5 +28,65 @@ def avg_bodymass(data):
         sex = row[i+1]
         body_mass = row[i-1]
 
-        if body_mass == '' or sex == '':
+
+
+
+    return results
+
+    pass
+
+
+def test_calc1(): #test case for calculation 1 (avg boddy mass)
+    #gen1
+    data = [
+        {'species': 'Adelie', 'sex': "male", 'body_mass': '4000'},
+        {'species': 'Adelie', 'sex': "male", 'body_mass': '6000'}
+    ]
+    result = avg_bodymass(data)
+    expected = 5000.00
+    if result.get(('Adelie', 'male')) == expected:
+        print('calc_avg_bodymass test passed')
+
+    else:
+        print(f'calc_avg_bodymass test failed: {result}')
+
+
+    #gen2
+    data = [
+        {'species': 'Adelie', 'sex': 'male', 'body_mass': '4000'},
+        {'species': 'Adelie', 'sex': 'female', 'body_mass': '3000'},
+        {'species': 'Gentoo', 'sex': 'male', 'body_mass': '5000'}
+    ]
+    result = avg_bodymass(data)
+    if (result.get(('Adelie', 'male')) == 4000.00 and
+        result.get(('Adelie', 'female')) == 3000.00 and
+        result.get(('Gentoo', 'male')) == 5000.00):
+        print('calc_avg_bodymass test passed')
+    else:
+        print(f'calc_avg_bodymass test failed: {result}')
+
+
+    #edge1
+    data = [
+        {'species': 'Adelie', 'sex': 'male', 'body_mass': 'NA'},
+        {'species': 'Adelie', 'sex': 'male', 'body_mass': '5000'}
+    ]
+    result = avg_bodymass(data)
+    if result.get(('Adelie', 'male')) == 5000.00:
+        print('calc_avg_bodymass test passed')
+    else:
+        print(f'calc_avg_bodymass test failed: {result}')
+
+
+    #edge2
+    data = [
+        {'species': 'Adelie', 'sex': 'male', 'body_mass': 'NA'},
+    ]
+    result = avg_bodymass(data)
+    if result == {}:
+        print('calc_avg_bodymass test passed')
+    else:
+        print(f'calc_avg_bodymass test failed: {result}')
+
+def test_calc2(): #test case for calculation 2 (flipper length > 200mm)
 
